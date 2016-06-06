2087b61d591bc810ba9b1a7651c9ff26e8715f27
CEH <ceh@itfactory.neta>
2016-06-06 18:51:30 +0200

    AngularJS alapok és Include

---
+++ src/js/controller/bodyController.js
@@ -0,0 +1,29 @@
+// Body kontrollere.
+webapp.controller( "bodyController", ['$scope', '$http', 'userFactory',
+    function($scope, $http, userFactory){
+        
+    $scope.isLoggedIn = false;        
+        
+    $scope.name = "Jeffrey";
+        
+    $scope.users = [];
+        
+    // Bejelentkezés.
+    $scope.doLogin = function() {
+        if ( !$scope.loginData ) {
+            alert('Kérjük töltse ki a mezőket!');
+            return;
+        }
+        if ( !$scope.loginData.email || !$scope.loginData.pass ) {
+            alert('Kérjük töltse ki a mezőket!');
+            return;
+        }
+        
+        userFactory.checkLogin($scope.loginData)
+            .then(function(loggedIn){
+                $scope.isLoggedIn = loggedIn;
+            });
+    }
+        
+          
+}]);
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,2 @@
-var reloaded = angular.module( "reloaded", [] );
-reloaded.controller( "hello", ['$scope', '$http',
-    function($scope, $http){
-    $scope.name = "Jeffrey";
-        
-    $scope.users = [];
-    $http.get( 'json/user.json' )
-        .then( function(serverData) {
-            $scope.users = serverData.data;
-        });
-        
-}]);
+// Az angular fő-modul létrehozása.
+var webapp = angular.module( "webapp", [] );
